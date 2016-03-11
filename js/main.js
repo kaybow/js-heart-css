7705fda3037a363514072ba5337a568bf9562e0b
kaybow <k.bowmaster@hotmail.com>
2016-03-11 16:38:00 -0500

    Added variables, added changes to classes

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main');
+var $ = $('p');
+var $ = $('h2');
+var $ = $('main li');
+var $ = $('dl');
+var $ = $('dt');
+var $ = $('img');
+
+$header.addClass('.masthead');
+$h1.removeClass('.big-heading')
+$h1.addClass('.logo')
