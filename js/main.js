1fd1e857d16ac77bf351f3ff261b7a7c35968a4e
kaybow <k.bowmaster@hotmail.com>
2016-03-11 16:47:50 -0500

    Adding and removing classes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 var $header = $('header');
 var $h1 = $('h1');
 var $main = $('main');
-var $ = $('p');
-var $ = $('h2');
-var $ = $('main li');
-var $ = $('dl');
+var $p = $('p');
+var $h2 = $('h2');
+var $img = $('img');
 var $ = $('dt');
 var $ = $('img');
 
 $header.addClass('.masthead');
-$h1.removeClass('.big-heading')
-$h1.addClass('.logo')
+$h1.removeClass('.big-heading');
+$h1.addClass('.logo');
+$main.addClass('.wrapper');
+$p('main p: first-child').addClass('.intro');
+$h2.addClass('.slug-head');
+$main('main li').addClass('slug-list');
+$img.addClass('slug-img');
